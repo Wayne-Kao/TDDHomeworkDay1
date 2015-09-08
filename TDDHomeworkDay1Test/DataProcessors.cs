5c9f085413543752e3b619745ca7073902d1ba9e
WayneKao <tai.wayne218@gmail.com>
2015-09-09 00:45:14 +0800

    upload files

---
+++ TDDHomeworkDay1Test/DataProcessors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TDDHomeworkDay1Test
+{
+    public class DataProcessors
+    {
+        public int[] GroupAndSum<T>(List<T> sourceDatas, string propertyName, int numberOfGroups)
+        {
+            List<int> result = new List<int>();
+            int groupSum = 0;
+            PropertyInfo pinfo = typeof(T).GetProperty(propertyName);
+            for (int i = 0; i < sourceDatas.Count(); i++)
+			{
+                int propertyValue;
+                //確保資料型別是數字
+                if (int.TryParse(pinfo.GetValue(sourceDatas.ElementAt(i)).ToString(), out propertyValue))
+                {
+                    groupSum += propertyValue;
+                }
+
+                //整除 或 最後一筆資料
+                if (((i + 1) % numberOfGroups) == 0 || (i == sourceDatas.Count() - 1))
+                {
+                    result.Add(groupSum);
+                    //歸0
+                    groupSum = 0;
+                }
+			}
+            return result.ToArray();
+        }
+    }
+}
---
+++ TDDHomeworkDay1Test/DataProcessorsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDHomeworkDay1Test
+{
+    [TestClass]
+    public class DataProcessorsTest
+    {
+        [TestMethod]
+        public void 驗證三筆一組取得Cost欄位總和()
+        {
+            //arrange
+            var target = new DataProcessors();
+            var sourceDatas = new List<TestDataModel> {            
+                new TestDataModel() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                new TestDataModel() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                new TestDataModel() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                new TestDataModel() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                new TestDataModel() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                new TestDataModel() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                new TestDataModel() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                new TestDataModel() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                new TestDataModel() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                new TestDataModel() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                new TestDataModel() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
+            };
+            int numberOfGroups = 3;
+            string propertyName = "Cost";
+            var expected = new int[] { 6, 15, 24, 21 };
+
+            //act
+            var actual = target.GroupAndSum(sourceDatas, propertyName, numberOfGroups);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 驗證四筆一組取得Revenue欄位總和()
+        {
+            //arrange
+            var target = new DataProcessors();
+            var sourceDatas = new List<TestDataModel> {            
+                new TestDataModel() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                new TestDataModel() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                new TestDataModel() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                new TestDataModel() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                new TestDataModel() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                new TestDataModel() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                new TestDataModel() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                new TestDataModel() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                new TestDataModel() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                new TestDataModel() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                new TestDataModel() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
+            };
+            int numberOfGroups = 4;
+            string propertyName = "Revenue";
+            var expected = new int[] { 50, 66, 60 };
+
+            //act
+            var actual = target.GroupAndSum(sourceDatas, propertyName, numberOfGroups);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        public class TestDataModel
+        {
+            public int Id { get; set; }
+            public int Cost { get; set; }
+            public int Revenue { get; set; }
+            public int SellPrice { get; set; }
+        }
+    }
+}
